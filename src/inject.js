12d2f3e47dabab6e115a4fc49d037b7863494361
Alexander Pirsig <apirsig@web.de>
2013-03-26 14:57:17 +0100

    cleanup code

--- src/inject.js
+++ src/inject.js
@@ -1,21 +1,10 @@
-;(function($,c){
+;(function($){
 
   window.addEventListener('message', function(event){
 
-    /*
-    window.jQuery(document).hshakeify({
-      onEnd: function(){
-        console.log('the end my friend');
-      },
-      css_file: null,
-      audio_file: 'http://piscis.testdrive.cc/jquery-harlem-shake-plugin/harlemshake'
-    });
-    */
-
-
     if(event.data && event.data.type=='hs-shake-config'){
 
-      window.jQuery(document).hshakeify({
+      $(document).hshakeify({
         onEnd: function(){
           console.log('the end my friend');
         },
@@ -24,7 +13,6 @@
       });
     }
 
-    //console.log('page javascript got message:', event);
   });
 
 })(jQuery,chrome);
